39ad268d47440617bf3b8846534adfced9599c3d
Jst77 <justinte7@gmail.com>
2021-11-04 09:37:54 -0500

    deliverable finished

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {    
+    res.send('Hello, World!');});
+    
+app.listen(3000);
+
+app.get('/frasier', function (req, res) {
+    res.send('Once in prep school, the Existentialist Club once named me Most Likely to Be')    
+  })
+
+app.get('/niles', function (req, res) {
+    res.send('You know, sometimes I wonder if Im not just in psychiatry for the money')    
+  })
+
+  app.get('/daphne', function (req, res) {
+    res.send("Well, have fun.  I'm off to stick my head in the oven.")    
+  })
+
+  app.get('/roz', function (req, res) {
+    res.send("Let me make it easy for you.  Freaks! Freaks on line 1. Freaks on line 2! Freaks! Everywhere!")    
+  })
+
+  app.get('/martin', function (req, res) {
+    res.send("You may think it's tough being middle-aged, but think about me.  I got a son who's middle-aged.")    
+  })
+ 
+
+  app.get('/Scrappy', function (req, res) {
+    res.send("Woof")    
+  })
